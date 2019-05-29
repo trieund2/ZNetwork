f0fd4f9776843654a77488b5fb5313c3b92ec09b
trieund2 <trieund2@vng.com.vn>
2019-05-29 14:44:58 +0700

    add ZADefine

---
+++ ZNetwork/ZNetwork/ZACore/ZADefine.h
@@ -0,0 +1,13 @@
+//
+//  ZADefine.h
+//  ZNetwork
+//
+//  Created by CPU12202 on 5/29/19.
+//  Copyright © 2019 com.trieund. All rights reserved.
+//
+
+#ifndef ZDefine_h
+#define ZDefine_h
+
+
+#endif /* ZDefine_h */
