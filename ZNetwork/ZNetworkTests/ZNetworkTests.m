adc73bbece2ec1fc8d81a314a2f5f9a016be19c1
Nguyen Thanh Ha <nguyen.thanh.ha@mqsolutions.com.vn>
2019-06-03 16:14:40 +0700

    Add a unit test multi commit task info

--- ZNetwork/ZNetworkTests/ZNetworkTests.m
+++ ZNetwork/ZNetworkTests/ZNetworkTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "ZASessionStorage.h"
 
 @interface ZNetworkTests : XCTestCase
 
@@ -14,24 +15,13 @@
 
 @implementation ZNetworkTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testMultiCommitTaskInfo {
+    NSString *testURLString = @"testURLString";
+    dispatch_apply(1000, dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^(size_t index) {
+        ZALocalTaskInfo *taskInfo = [[ZALocalTaskInfo alloc] initWithURLString:testURLString filePath:@"testFilePaht" fileName:@"testFileName" countOfTotalBytes:10000];
+        [[ZASessionStorage sharedStorage] commitTaskInfo:taskInfo];
+    });
+    XCTAssertTrue([[ZASessionStorage sharedStorage] containsTaskInfo:testURLString]);
 }
 
 @end
