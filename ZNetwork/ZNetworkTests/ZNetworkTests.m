21ed789298b45459ed6ad2b8a759f5b0b6b33f08
Nguyen Thanh Ha <nguyen.thanh.ha@mqsolutions.com.vn>
2019-06-03 17:55:39 +0700

    Add unit test count of task info

--- ZNetwork/ZNetworkTests/ZNetworkTests.m
+++ ZNetwork/ZNetworkTests/ZNetworkTests.m
@@ -46,4 +46,26 @@
     XCTAssertTrue(![[ZASessionStorage sharedStorage] containsTaskInfo:testURLString3]);
 }
 
+- (void)testCountOfTaskInfo {
+    dispatch_apply(1000, dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^(size_t index) {
+        ZALocalTaskInfo *taskInfo = [[ZALocalTaskInfo alloc] initWithURLString:@"testURLString" filePath:@"testFilePaht" fileName:@"testFileName" countOfTotalBytes:10000];
+        [[ZASessionStorage sharedStorage] commitTaskInfo:taskInfo];
+    });
+    XCTAssertEqual([[ZASessionStorage sharedStorage] countOfTaskInfo], 1);
+    
+    NSString *testURLString1 = @"testURLString1";
+    NSString *testURLString2 = @"testURLString2";
+    ZALocalTaskInfo *taskInfo1 = [[ZALocalTaskInfo alloc] initWithURLString:testURLString1
+                                                                   filePath:@"testFilePath"
+                                                                   fileName:@"testFileName"
+                                                          countOfTotalBytes:10000];
+    ZALocalTaskInfo *taskInfo2 = [[ZALocalTaskInfo alloc] initWithURLString:testURLString2
+                                                                   filePath:@"testFilePath"
+                                                                   fileName:@"testFileName"
+                                                          countOfTotalBytes:10000];
+    [[ZASessionStorage sharedStorage] commitTaskInfo:taskInfo1];
+    [[ZASessionStorage sharedStorage] commitTaskInfo:taskInfo2];
+    XCTAssertEqual([[ZASessionStorage sharedStorage] countOfTaskInfo], 3);
+}
+
 @end
