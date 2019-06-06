5f2fa72780fed893cf465a0f15d1c3e0d04ea2ca
trieund2 <trieund2@vng.com.vn>
2019-06-06 19:38:21 +0700

    Refactor UniTest

---
+++ ZNetwork/ZNetworkTests/ZNetworkConstant.m
@@ -0,0 +1,15 @@
+//
+//  ZNetworkConstant.m
+//  ZNetworkTests
+//
+//  Created by CPU12202 on 6/6/19.
+//  Copyright © 2019 com.trieund. All rights reserved.
+//
+
+#import "ZNetworkConstant.h"
+
+const int64_t kAsyncTestTimeout = 90;
+const int64_t kSmallDelayNanosecond = NSEC_PER_SEC * 1000; // 1s
+const int64_t kMediumlDelayNanosecond = NSEC_PER_SEC * 3000; // 3s
+const int64_t kLargeDelayNanosecond = NSEC_PER_SEC * 5000; // 5s
+const NSString *kFileURLString1GB = @"http://ipv4.download.thinkbroadband.com/1GB.zip";
